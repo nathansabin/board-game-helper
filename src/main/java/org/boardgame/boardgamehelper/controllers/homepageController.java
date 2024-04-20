1e29e644e94dd638d87f25521ac4c2974b80cd41
nathansabin <sabinnat001@gmail.com>
2024-04-19 21:59:24 -0700

    adjusted header, layed out feature bar in main menu

--- src/main/java/org/boardgame/boardgamehelper/controllers/homepageController.java
+++ src/main/java/org/boardgame/boardgamehelper/controllers/homepageController.java
@@ -1,5 +1,21 @@
 package org.boardgame.boardgamehelper.controllers;
 
-public class homepageController {
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
 
+public class homepageController {
+    @FXML
+    private Pane headerRoot;
+
+    @FXML
+    private Rectangle headerBox;
+    @FXML
+    private Rectangle menuBox;
+
+    public void initialize() {
+        headerBox.widthProperty().bind(headerRoot.widthProperty());
+        menuBox.heightProperty().bind(headerRoot.heightProperty());
+    }
 }
